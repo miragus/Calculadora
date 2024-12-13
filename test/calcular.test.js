de1a8a8098bef8b0af6462fad6a253dba2af7d13
RHUAN GUSTAVO MIRANDA DE LIMA <a95288486@rn.senac.local>
2024-12-13 16:46:57 -0300

    13/12

---
+++ test/calcular.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { calcularResultado } from "../js/calcular";
+
+describe('testes em uma aplicação electron', () => {
+
+    it('deve calcular um resultado', () => {
+
+    });
+})
